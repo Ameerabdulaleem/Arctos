2376a8480d11cc8d296e1a85dac39c103d5e3321
Ameer Abdulaleem <ameerabdulaleem564@gmail.com>
2026-02-12 14:46:42 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/app/components'),
-    },
-  },
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase from 500kB to 1000kB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          email: ['nodemailer'], // Split nodemailer into separate chunk
+        }
+      }
+    }
+  }
 })
